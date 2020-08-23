068b744810a715275502d600a1de155fbf292e41
Bryce Mardon <brycemardon@Bryces-MBP.modem>
2020-08-23 21:35:58 +1000

    new files

--- src/App.js
+++ src/App.js
@@ -1,29 +1,64 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import {Button, FormControl, InputLabel, Input} from '@material-ui/core/';
 import './App.css';
+import Message from './Message';
+import db from './firebase';
+
 
 function App() {
   const [input, setInput] = useState('')
-  const [messages, setMessages] = useState(['hello', 'Hi', 'Whats up']);
+  const [messages, setMessages] = useState([
+    {username: 'Bryce', message: 'Hi'}, 
+    {username: 'Sarah', message: 'Hello'}
+  ]);
+  const [username, setUsername] = useState('');
 
-  console.log(input);
-  console.log(messages);
+  // useState = variable in REACT
+  // useEffect = run code on a condition in REACT
+
+  useEffect(() => {
+    // run once when the app component loads
+    db.collection('messages').onSnapshot(snapshot => {
+      setMessages(snapshot.docs.map(doc => doc.data()))
+    })
+  }, [] )
+
+  useEffect(() => {
+    // const username = prompt('Please enter your name')
+    setUsername(prompt('Please enter your name'))
+
+    //run code here...
+    // if its blank inside [], this code runs ONCE when the app component loads
+  }, [] ) //condition
 
   const sendMessage = (event) => {
-    // all the logic to send a message goes here
-    setMessages([...messages, input]);
+    event.preventDefault();
+    setMessages([
+      ...messages, 
+      { username: username, text: input}
+    ]);
     setInput('');
   }
 
   return (
     <div className="App">
       <h1>Facebook Messenger Clone 🚀 </h1>
+      <h2>Welcome {username} </h2>
 
-      <input value={input} onChange={event => setInput(event.target.value)}/>
-      <button onClick={sendMessage}>Send Message</button>
-      {/* input field */}
-      {/* button */}
+      <form>
+        <FormControl>
+          <InputLabel>Enter a message...</InputLabel>
+          <Input value={input} onChange={event => setInput(event.target.value)}/>
+          <Button disabled={!input} variant="contained" color="primary" type='submit' onClick={sendMessage}>Send Message</Button>
+        </FormControl>
+      </form>
+      
+      {
+        messages.map(message => (
+          <Message username={username} message={message} />
+        ))
+      }
 
-      {/* messages themselves */}
     </div>
   );
 }
