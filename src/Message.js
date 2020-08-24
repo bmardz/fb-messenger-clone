6878dba573dc79447bc3d069bdae1c086a2459ca
Bryce Mardon <brycemardon@Bryces-MBP.modem>
2020-08-24 21:46:12 +1000

    updated files

--- src/Message.js
+++ src/Message.js
@@ -6,7 +6,7 @@ const Message = forwardRef(({ message, username }, ref) => {
     const isUser = username === message.username;
 
     return (
-        <div className={`message ${isUser && 'message__user'}`}>
+        <div ref={ref} className={`message ${isUser && 'message__user'}`}>
             <Card className={isUser ? "message__userCard" : "message__guestCard"}>
                 <CardContent>
                     <Typography
@@ -14,7 +14,7 @@ const Message = forwardRef(({ message, username }, ref) => {
                         variant="h5"
                         component="h2"
                     >
-                        {message.username}: {message.message}
+                        {!isUser && `${message.username || 'Unknown User'}:`} {message.message}
                     </Typography>
                 </CardContent>
             </Card>
